65b97bad721fad367023a3420f584197cb8df79a
Questgig <90308279+learnwithalex@users.noreply.github.com>
2023-01-03 06:30:30 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require('config.php');
 
 $query = urlencode($_GET["YOUR PARAMETER NAME"]);
-$url = "https://questgig.com/cloud/bigdata/search-api/?query=$query&api_key=API_KEY";
+$url = "https://apis.questgig.com/cloud/v2/search-api/?query=$query&api_key=API_KEY";
 $response = file_get_contents($url);
 print($response);
 
